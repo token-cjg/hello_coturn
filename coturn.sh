2f4e1e74ff1185687345c6b68b3c3bd22511ee36
Christopher Goddard <chrisjgoddard@gmail.com>
2020-10-05 07:07:50 +1100

    Set coturn to daemon mode

--- coturn.sh
+++ coturn.sh
@@ -5,3 +5,8 @@ sudo apt-get -y update
 sudo apt-get -y install coturn
 
 sudo systemctl stop coturn
+
+# set coturn to daemon mode
+sudo mv /etc/default/coturn /etc/default/coturn.orig
+curl -O -L https://raw.githubusercontent.com/token-cjg/hello_coturn/master/fixtures/coturn
+sudo mv coturn /etc/default
